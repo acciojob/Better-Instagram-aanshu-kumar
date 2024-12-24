e96bbaf6f5acdb9af6c5ebbbaf61a95bbd3e4037
Anshu kumar <40786793+aanshu-kumar@users.noreply.github.com>
2024-12-24 18:23:57 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,21 @@
 //your code here
+
+let initial=null;
+let final=null;
+
+function dragStart(e){
+    initial=e.target;
+}
+function onDrop(e){
+    final=e.target;
+    let temp=window.getComputedStyle(initial, null).getPropertyValue("background-image");
+    initial.style.backgroundImage=window.getComputedStyle(final, null).getPropertyValue("background-image");
+    final.style.backgroundImage=temp;
+    initial=null;
+    final=null;
+    temp=null;
+}
+
+function dragOver(e){
+    e.preventDefault();
+}
